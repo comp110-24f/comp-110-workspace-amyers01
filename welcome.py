b7c5f2f2888e76e40ff406c73fef8e10a2f16d90
Alexandra Myers <amyers01@unc.edu>
2024-08-19 23:07:58 -0400

    Welcome Message

--- welcome.py
+++ welcome.py
@@ -5,7 +5,3 @@ __author__ = "01234567"
 print("Welcome to COMP110!")
 print("You are in for a fun adventure into programming!")
 print("<3 the COMP110 Team!")
-
-$ git config --global user.name "Alexandra Myers"
-$ git config --global user.email amyers01@unc.edu
-
