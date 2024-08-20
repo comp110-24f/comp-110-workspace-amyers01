9c4dda04c6a3d8c49ed7ec42e67ec9796997b9ad
Alexandra Myers <amyers01@unc.edu>
2024-08-19 23:08:46 -0400

    Do not need

--- exercises 2/ex00_hello_world.py
+++
@@ -1,8 +0,0 @@
-"""My first exercise in COMP110!"""
-
-__author__ = "730654167"
-
-
-def greet(name: str) -> str:
-    """A welcoming first function definition."""
-    return "Hello, " + name + "!"
