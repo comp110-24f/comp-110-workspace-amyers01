cd24d1bb4767700b656279648581721fc0259693
Alexandra Myers <amyers01@unc.edu>
2024-09-27 13:03:23 -0400

    Challenge Question 02

---
+++ cqs/cq02_conditionals.py
@@ -0,0 +1,19 @@
+"""My second challenge question, focusing on conditionals!"""
+
+__author__ = "730654167"
+
+
+def guess_a_number() -> None:
+    secret: int = 7
+    guess: int = int(input("Guess a number: "))
+    print("Your guess was " + str(guess))
+    if guess == secret:
+        print("You got it!")
+    elif guess < secret:
+        print("Your guess was too low! The secret number is " + str(secret))
+    else:
+        print("Your guess was too high! The secret number is " + str(secret))
+
+
+if __name__ == "__main__":
+    guess_a_number()
