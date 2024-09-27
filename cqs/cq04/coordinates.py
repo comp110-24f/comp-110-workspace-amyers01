2ddf6c595330da7202276aca17f592a757b1a9de
Alexandra Myers <amyers01@unc.edu>
2024-09-27 13:02:39 -0400

    Challenge Question 04

--- cqs/cq04/coordinates.py
+++ cqs/cq04/coordinates.py
@@ -4,14 +4,13 @@ __author__ = "730654167"
 
 
 def get_coords(xs: str, ys: str) -> None:
-    xind: int = 0
-    yind: int = 0
-    while xind < len(xs):
-        while yind < len(ys):
-            print(f"({xs[xind]},{ys[yind]})")
-            yind += 1
-        xind += 1
-        yind = 0
+    x_idx: int = 0
+    while x_idx < len(xs):
+        y_idx: int = 0
+        while y_idx < len(ys):
+            print(f"({xs[x_idx]},{ys[y_idx]})")
+            y_idx += 1
+        x_idx += 1
 
 
 if __name__ == "__main__":
