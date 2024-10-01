96917a8a95296d72c9c04baa8d1d175b30fb1b9d
Alexandra Myers <amyers01@unc.edu>
2024-10-01 15:17:49 -0400

    Lists Practice

--- lessons/lists_practice.py
+++ lessons/lists_practice.py
@@ -6,33 +6,33 @@ my_numbers: list[float] = []  # using a literal
 
 my_numbers.append(1.5)
 
-# print(my_numbers)
+print(my_numbers)
 
 # String Analogy
-# my_name: str = "" # literal
-# my_name: str = str() # constructor
+my_name: str = ""  # literal
+my_name: str = str()  # constructor
 
 
 # Creating an already populated list
 game_points: list[int] = [102, 86, 94]
-# print(game_points)
+print(game_points)
 
 # Subscription Notation/Indexing
-# print([102, 86, 94][2])  # option 1
+print([102, 86, 94][2])  # option 1
 last_game: int = game_points[2]  # option 2
-# print(last_game)
-# print(game_points[2])  # option 3
+print(last_game)
+print(game_points[2])  # option 3
 
 # Modifying by Index
 game_points[1] = 72
-# print(game_points)
+print(game_points)
 
 # Getting the length
 len(game_points)
 
 # Removing an item
 game_points.pop(1)
-# print(game_points)
+print(game_points)
 
 # function name: display
 # parameter : list of integers
@@ -50,4 +50,4 @@ def display(int_list: list[int]) -> None:
         index += 1
 
 
-# display(int_list=game_points)
+display(int_list=game_points)
