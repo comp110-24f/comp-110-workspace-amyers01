07264f4b24ee047898f2d980b5edf312aab13816
Alexandra Myers <amyers01@unc.edu>
2024-10-20 23:12:11 -0400

    Progress on Exercise 05

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,53 @@
+"""Exercise 05 - Implementing list utility functions"""
+
+__author__ = "730654167"
+
+
+def only_evens(input: list[int]) -> list[int]:
+    """Creates a new list containing only the even elements of the input list"""
+    even: list[int] = []
+    for num in input:
+        if num % 2 == 0:
+            even.append(num)
+        num += 1
+    return even
+
+
+# even is an empty list, taking on the elements that are even from input
+
+
+def sub(input: list[int], start: int, end: int) -> list[int]:
+    """Creates a subset list of the input list between start and end indexes"""
+    if start < 0:
+        start = 0
+    if end > len(input):
+        end = len(input)
+    if len(input) == 0 or start >= len(input) or end <= 0:
+        return []
+    return input[start:end]
+
+
+# takes the value of the list with start index and value of the list with the end index
+# makes a subset list of those variables, plus the varaibles between
+# ([start:end]) == start index to end index (not including)
+
+
+def add_at_index(input: list[int], elem: int, idx: int) -> None:
+    """Modifies the input list to place the element at the given index"""
+    if idx < 0 or idx > len(input):
+        raise IndexError("Index is out of bounds for the input list")
+    input.append(elem)
+    for insert in range(len(input) - 1, idx, -1):
+        input[insert] = input[insert - 1]
+    input[idx] = elem
+
+
+# if the index argument is less than 0 or greater than the length = error
+# add elem to the end of the list
+# iterates over each element in the list, starting from the end of the list
+# start: len(input) -1 = length of list is 5, then the last index value is 4
+# end: at the index value argument
+# step to each "insert" value by -1 = going backwards by 1, since starting at end
+# input[insert] value becomes input[insert-1] - value to left
+# if 0 is at input[3], and 4 is to the left at input[2], assign 4 to input[3] = input[3-1]
+# once get to the index argument value, assign that input[idx] = elem
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,79 @@
+"""Exercise 05 - defining unit tests for utility functions"""
+
+__author__ = "730654167"
+
+from exercises.ex05.utils import only_evens, sub, add_at_index
+
+
+def test_return_only_evens() -> None:
+    """Ensures that only_evens returns the expected value"""
+    a: list[int] = [1, 2, 3, 4]
+    assert only_evens(a) == [2, 4]
+
+
+def test_process_only_evens() -> None:
+    """Ensures that only_evens does not mutate, but has desired effect"""
+    a: list[int] = [1, 2, 3, 4]
+    even: list[int] = only_evens(a)
+    assert even == [2, 4]
+
+
+def test_edge_only_evens() -> None:
+    """Ensures correct value is returned on a list with no evens"""
+    a: list[int] = [7, 11, 5]
+    assert only_evens(a) == []
+
+
+def test_return_sub() -> None:
+    """Ensures that sub returns the expected value"""
+    a: list[int] = [23, 24, 37, 44]
+    first: int = 1
+    last: int = 3
+    assert sub(a, first, last) == [24, 37]
+
+
+def test_process_sub() -> None:
+    """Ensures that sub does not mutate, but has desired effect"""
+    a: list[int] = [23, 24, 37, 44]
+    first: int = 1
+    last: int = 3
+    sub(a, first, last)
+    assert a[first:last] == [24, 37]
+
+
+def test_edge_sub() -> None:
+    """Ensures correct value is returned on an unconvential list, start, or end"""
+    a: list[int] = []
+    first: int = 7
+    last: int = 2
+    sub(a, first, last)
+    assert sub(a, first, last) == []
+
+
+def test_return_add_idx() -> None:
+    """Ensures that add_at_index returns the expected value"""
+    a: list[int] = [7, 9, 10, 15]
+    num: int = 8
+    index: int = 1
+    assert add_at_index(a, num, index) is None
+
+
+def test_process_add_idx() -> None:
+    """Ensures that add_at_index has the desired effect"""
+    a: list[int] = [7, 9, 10, 15]
+    num: int = 8
+    index: int = 1
+    add_at_index(a, num, index)
+    assert a == [7, 8, 9, 10, 15]
+
+
+import pytest
+
+
+def test_edge_add_idx() -> None:
+    """Ensures correct value is returned on an invalid index"""
+    a: list[int] = [1, 4]
+    num: int = 8
+    index: int = 4
+    with pytest.raises(IndexError):
+        add_at_index(a, num, index)
